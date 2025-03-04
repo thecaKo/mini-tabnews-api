186a41e254d83790ee10bb9088461a4445645280
caKo <cakoonrails@gmail.com>
2025-03-03 23:25:14 -0300

    adding JWT/COOKIES and authenticate route

---
+++ src/http/users/authenticate.spec.ts
@@ -0,0 +1,32 @@
+import { app } from "@/app";
+import { startTestEnvironment, teardownTestDatabase } from "prisma/vitest-environment-prisma/setup";
+import request from "supertest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+
+describe("Authenticate (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+    await startTestEnvironment();
+  });
+  afterAll(async () => {
+    await app.close();
+    await teardownTestDatabase();
+  });
+
+  it("should be able to authenticate", async () => {
+    await request(app.server).post("/users").send({
+      name: "cako",
+      email: "cakinho@gmail.com",
+      password: "123456",
+    });
+
+    const response = await request(app.server).post("/sessions").send({
+      email: "cakinho@gmail.com",
+      password: "123456",
+    });
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    });
+  });
+});
---
+++ src/http/users/authenticate.ts
@@ -0,0 +1,54 @@
+import { PrismaUserRepository } from "@/repositories/prisma/prisma-users-repository";
+import { AuthenticateService } from "@/services/authenticate";
+import { InvalidCredentialsError } from "@/services/errors/invalid-credentials-error";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
+  const authenticateBodySchema = z.object({
+    email: z.string(),
+    password: z.string().min(6),
+  });
+
+  const { email, password } = authenticateBodySchema.parse(request.body);
+
+  try {
+    const prismaUsersRepository = new PrismaUserRepository();
+    const authenticateService = new AuthenticateService(prismaUsersRepository);
+    const { user } = await authenticateService.execute({ email, password });
+
+    const token = await reply.jwtSign(
+      { role: user.role },
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    );
+
+    const refreshToken = await reply.jwtSign(
+      { role: user.role },
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: "7d",
+        },
+      },
+    );
+
+    return reply
+      .setCookie("refreshToken", refreshToken, {
+        path: "/",
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({ token: token });
+  } catch (err) {
+    if (err instanceof InvalidCredentialsError) {
+      return reply.status(400).send();
+    }
+    throw err;
+  }
+}
